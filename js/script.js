5284f3c7d31101537e70d9ba33e8b74af85b2e5f
Valentyn <wolf.valentyn@gmail.com>
2022-06-30 17:00:18 +0300

    delete comments

--- js/script.js
+++ js/script.js
@@ -52,27 +52,25 @@ let products = {
 };
 
 for (let i of products.data) {
-    // Create card
     let card = document.createElement("div");
-    // card should have category and should stay hidden innitially
     card.classList.add("card", i.category);
-    // Image div
+
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("image-container");
-    // img tag
+
     let image = document.createElement("img");
     image.setAttribute("src", i.image);
     imgContainer.appendChild(image);
     card.appendChild(imgContainer);
-    // container
+
     let container = document.createElement("div");
     container.classList.add("container");
-    // product name 
+
     let name = document.createElement("h5");
     name.classList.add("product-name");
     name.innerText = i.productName.toLocaleUpperCase();
     container.appendChild(name);
-    // price
+
     let price = document.createElement("h6");
     price.innerText = "$" + i.price;
     container.appendChild(price);
@@ -81,13 +79,10 @@ for (let i of products.data) {
     document.getElementById("products").appendChild(card);
 }
 
-// parameter passed from button (Parameter same as category)
-
 function filterProducts (value) {
-    // Button class code
     let buttons = document.querySelectorAll(".button-value");
+
     buttons.forEach(button => {
-        // check if value equals innerText
         if (value.toUpperCase() == button.innerText.toUpperCase()) {
             button.classList.add("active");
         } else {
@@ -95,48 +90,35 @@ function filterProducts (value) {
         }
     });
 
-    // select all cards
     let elements = document.querySelectorAll(".card");
-    // loop through all cards 
+    
     elements.forEach(element => {
-        // display all cards on 'all' button click
         if (value == "All") {
             element.classList.remove("hide");
         } else {
-            // Check if element contains category class
             if (element.classList.contains(value)) {
-                // display element based on category 
                 element.classList.remove("hide")
             } else {
-                // hide other elements 
                 element.classList.add("hide");
             }
         }
     });
 }
 
-// Search button click 
 document.getElementById("search").addEventListener("click", () => {
-    // innitializations
     let searchInput = document.getElementById("search-input").value;
     let elements = document.querySelectorAll(".product-name");
     let cards = document.querySelectorAll(".card");
-    /* console.log(searchInput); */
 
-    // loop through all elements
     elements.forEach((element, index) => {
-        // check if text include the search value
         if (element.innerText.includes(searchInput.toUpperCase())) {
-            // display matching card
             cards[index].classList.remove("hide");
         } else {
-            // hide others
             cards[index].classList.add("hide");
         }
     })
 });
 
-// Initially display all products
 window.onload = () => {
     filterProducts("All");
 }
